9b503120d791d20e77c413eaa618cbce90005982
Daniel <skalskidaniel@icloud.com>
2024-05-08 10:02:56 +0200

    Done

--- kmp.cpp
+++ kmp.cpp
@@ -28,13 +28,64 @@ vector<int> get_lps(string s)
     }
     return LPS;
 }
+void kmp(string pattern, string text)
+{
+    if (pattern.size() > text.size())
+    {
+        return;
+    }
+    else
+    {
+        if (pattern.size() == text.size())
+        {
+            if (pattern == text)
+                cout << 0 << endl;
+            else
+                return;
+        }
+        if (pattern == "")
+        {
+            cout << 0 << endl;
+            return;
+        }
+        vector<int> lps = get_lps(text);
+        int i = 0, j = 0;
+        while (i < text.size())
+        {
+            if (text[i] == pattern[j])
+            {
+                ++i;
+                ++j;
+            }
+            if (j == pattern.size())
+            {
+                cout << i - j << endl;
+                j = lps[j - 1];
+            }
+            else if (i < text.size() && text[i] != pattern[j])
+            {
+                if (j != 0)
+                {
+                    j = lps[j - 1];
+                }
+                else
+                {
+                    ++i;
+                }
+            }
+        }
+    }
+}
 
 int main() {
     int t;
     cin >> t;
     while(t--)
     {
-        
+        int n;
+        string pattern, text;
+        cin >> n >> pattern >> text;
+        kmp(pattern, text);
     }
 
 }
