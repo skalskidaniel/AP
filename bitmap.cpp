ecc593b836ceac0f9c4743134b07192d68462623
Daniel <skalskidaniel@icloud.com>
2024-05-13 21:41:35 +0200

    Done with massive complexity

--- bitmap.cpp
+++ bitmap.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+
+#define MAX 128*128
+
 using namespace std;
 
 struct Pixel
@@ -17,6 +20,18 @@ int distance(Pixel p1, Pixel p2)
     return abs(p1.x - p2.x) + abs(p1.y - p2.y);
 }
 
+int lowest_distance(Pixel p1, vector<Pixel> white_pixels)
+{
+    int lowest = MAX;
+    for (auto p2 : white_pixels)
+    {
+        int current_distance = distance(p1, p2);
+        if (current_distance < lowest)
+            lowest = current_distance;
+    }
+    return lowest;
+}
+
 int main()
 {
     int t;
@@ -27,14 +42,14 @@ int main()
         cin >> n >> m;
         int grid[n][m];
         vector<Pixel> white_pixels;
-        string line; // 1 means white - 0 means black
         for (int i = 0; i < n; ++i)
         {
+            string line; // 1 means white - 0 means black
+            cin >> line;
             for (int j = 0; j < m; ++j)
             {
-                int num;
-                cin >> num;
-                grid[n][m] = num;
+                int num = line[j] - '0';
+                grid[i][j] = num;
                 if (num == 1)
                 {
                     Pixel p;
@@ -44,6 +59,19 @@ int main()
                 }
 
             }
+        }
+
+        // now display dist to nearest white space for each pixel
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                Pixel p;
+                p.x = i;
+                p.y = j;
+                cout << lowest_distance(p, white_pixels) << " ";
+            }
+            cout << "\n";
         }
     }
 }
