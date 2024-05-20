3a16cbb3215dd33bac1d90e9666a648205a22cd1
Daniel <skalskidaniel@icloud.com>
2024-05-20 18:36:15 +0200

    Done

---
+++ candies.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'candies' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY arr
+ */
+
+long candies(int n, vector<int> arr)
+{
+    vector<int> candies(n, 1);
+
+    // Left to right pass
+    for (int i = 1; i < n; ++i)
+    {
+        if (arr[i] > arr[i - 1])
+        {
+            candies[i] = candies[i - 1] + 1;
+        }
+    }
+
+    // Right to left pass
+    for (int i = n - 2; i >= 0; --i)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            candies[i] = max(candies[i], candies[i + 1] + 1);
+        }
+    }
+
+    // Calculate the total number of candies
+    long totalCandies = 0;
+    for (int candy : candies)
+    {
+        totalCandies += candy;
+    }
+
+    return totalCandies;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        string arr_item_temp;
+        getline(cin, arr_item_temp);
+
+        int arr_item = stoi(ltrim(rtrim(arr_item_temp)));
+
+        arr[i] = arr_item;
+    }
+
+    long result = candies(n, arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+
+    return s;
+}
+
+string rtrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end());
+
+    return s;
+}
